8d26197f35f6d8d4e7f771b9b210d39be2c05485
Anusha Addagudi <anustudies.003@gmail.com>
2016-08-24 13:17:22 -0600

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,24 +1,14 @@
 #!/bin/python
-def insertionSort(ar):   
-    l = len(ar)
-    sorted_list = []
-    for i in range(1,l):
-        temp = ar[i]
-        for j in ar[:i][::-1]:
-            if j > temp:
-                ar.pop(ar.index(temp))
-                ar = ar[:ar.index(j)] + [temp] + ar[ar.index(j):]
-        sorted_list += ar
-    return sorted_list
+def insertion_sort(l):
+    for i in xrange(0, len(l)):
+        j = i-1 
+        key = l[i]
+        while (j >= 0) and (l[j] > key):
+            l[j+1] = l[j]
+            j -= 1
+        l[j+1] = key
 
 m = input()
 ar = [int(i) for i in raw_input().strip().split()]
-sorted_list = insertionSort(ar)
-temp_list = []
-i = 0
-while i < len(sorted_list):
-    temp_list.append(sorted_list[i:i+int(m)])
-    i += int(m)
-
-for item in temp_list: 
-    print " ".join(map(str, item))
+insertion_sort(ar)
+print " ".join(map(str,ar))
